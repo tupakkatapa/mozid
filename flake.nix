ec41dbd0330eb3c14c19aa30b9f5b4cbe1937e9d
tupakkatapa <jesse@ponkila.com>
2024-09-07 00:41:38 +0300

    flake: reword desc

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 # https://ertt.ca/nix/shell-scripts/
 {
-  description = "Mozid - retrieve Firefox extension IDs";
+  description = "Mozid - retrieve Firefox add-ons IDs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
